d1bb5526d5a09508ea1911496ba9e155adb0da33
SylvioDev <rakotomangantonio@gmail.com>
2025-11-07 09:55:06 +0300

    feat(checkout-process): Update checkout database model
    - Add order_number field for external order identification
    - Create OrderItem model
    - Rename user field into customer_id
    - Register Order and OrderItem models in admin page

--- apps/checkout/admin.py
+++ apps/checkout/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import (
+    PromoCode,
+    Order,
+    OrderItem
+)
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'customer_id', 'created_at', 'final_total']
+    list_filter = ['order_number', 'customer_id', 'created_at']
+    search_fields = ['order_number', 'customer_id']
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'product_name']
+    list_filter = ['order', 'product']
+    search_fields = ['product', 'order']
