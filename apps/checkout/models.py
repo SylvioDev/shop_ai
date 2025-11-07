88af70a661f97613aa0c763184c92b6589c045e8
SylvioDev <rakotomangantonio@gmail.com>
2025-11-07 09:55:06 +0300

    feat(checkout-process): Update checkout database model
    - Add order_number field for external order identification
    - Create OrderItem model
    - Rename user field into customer_id
    - Register Order and OrderItem models in admin page

--- apps/checkout/models.py
+++ apps/checkout/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from apps.products.models import Product
+import uuid
 class PromoCode(models.Model):
     
     DISCOUNT_TYPES = [
@@ -29,7 +30,8 @@ class Order(models.Model):
         ('cancelled', 'Cancelled')
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=255, unique=True, editable=False, default='ORD-2025')
+    customer_id = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -38,3 +40,23 @@ class Order(models.Model):
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2)
     final_total = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def save(self, force_insert = False , *args, **kwargs):
+        self.order_number = f'ORD-{self.created_at.year}-{uuid.uuid1().hex[:8].upper()}'
+        super().save(force_insert=force_insert, *args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.DecimalField(default=0.00, decimal_places=2, max_digits=10)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10, default=0.00)
+    product_name = models.CharField(max_length=255)
+    attributes = models.JSONField(verbose_name='product_attributes')
+    unit_price = models.DecimalField(decimal_places=2, max_digits=10)
+    quantity = models.IntegerField(default=1)
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+
+    
