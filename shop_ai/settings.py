692a30b1c1e1061f4f0221b345ca09b5cfbac111
SylvioDev <rakotomangantonio@gmail.com>
2025-12-15 18:22:41 +0300

    Update settings file

--- shop_ai/settings.py
+++ shop_ai/settings.py
@@ -88,10 +88,14 @@ WSGI_APPLICATION = 'shop_ai.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-    
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('PGDATABASE', 'shopai'),
+        'USER': os.environ.get('PGUSER', 'postgres'),
+        'PASSWORD': os.environ.get('PGPASSWORD', 'password'),
+        'HOST': os.environ.get('PGHOST', 'db'),  # Railway will override this
+        'PORT': os.environ.get('PGPORT', '5432'),
+    }
 }
 
 # Password validation
