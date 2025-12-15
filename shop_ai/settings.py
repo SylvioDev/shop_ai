50115ec7a9fad8908f13638af919892f1687e3d1
SylvioDev <rakotomangantonio@gmail.com>
2025-12-15 23:06:52 +0300

    Update settings file

--- shop_ai/settings.py
+++ shop_ai/settings.py
@@ -131,11 +131,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
+"""STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+"""
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
