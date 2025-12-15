52bc8ad717f9916403d35113e6cc4bdc25175f60
SylvioDev <rakotomangantonio@gmail.com>
2025-12-15 22:25:09 +0300

    Update settings file

--- shop_ai/settings.py
+++ shop_ai/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICIFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
