2d2e2867b4aba8e7e164e56016bf481d135a43f5
SylvioDev <rakotomangantonio@gmail.com>
2025-12-16 00:30:37 +0300

    Update settings file

--- shop_ai/settings.py
+++ shop_ai/settings.py
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "staticfiles",
 ]
 
-STATIC_ROOT = BASE_DIR / "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
