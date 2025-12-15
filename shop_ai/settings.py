6da4c2b204fd36f69035a30aa2e3f86c0b674c43
SylvioDev <rakotomangantonio@gmail.com>
2025-12-15 15:13:56 +0300

    Update settings file

--- shop_ai/settings.py
+++ shop_ai/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = ['localhost','127.0.0.1','shopai-229phlgj.b4a.run']#os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 # Application definition
 
