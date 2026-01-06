f46190d7f605e79685cf5acd44cb02e96efb7a41
SylvioDev <rakotomangantonio@gmail.com>
2026-01-06 12:43:43 +0300

    fix(settings):Clean production settings

--- shop_ai/settings/production.py
+++ shop_ai/settings/production.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['shopai-production-fac8.up.railway.app']
 
@@ -188,10 +188,10 @@ SOCIAL_AUTH_PIPELINE = (
 # Email-backend
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'ratremarakoto@gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 CART_SESSION_ID = "cart"
