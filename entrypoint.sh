eb49f7d7a621eabbd5013808fdfae60fbfe00bf1
SylvioDev <rakotomangantonio@gmail.com>
2026-01-19 20:47:08 +0300

    Modify settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,8 @@ export DJANGO_SETTINGS_MODULE=shop_ai.settings.production
 #echo "Applying database migrations..."
 #python manage.py migrate --noinput
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+#echo "Collecting static files..."
+#python manage.py collectstatic --noinput --clear
 
 echo "Starting Gunicorn..."
 exec gunicorn shop_ai.wsgi:application --bind 0.0.0.0:8000
