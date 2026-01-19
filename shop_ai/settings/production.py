1f6f82680f6c983127794c20f28b2cf3d9c8efc0
SylvioDev <rakotomangantonio@gmail.com>
2026-01-19 20:22:53 +0300

    Modify production settings

--- shop_ai/settings/production.py
+++ shop_ai/settings/production.py
@@ -20,7 +20,7 @@ import cloudinary
 load_dotenv() # Load environment variables from .env files
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
