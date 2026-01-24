e665def1429f3069fe045f315b07aca18be3c5eb
SylvioDev <rakotomangantonio@gmail.com>
2026-01-24 19:48:41 +0300

    Update DEBUG variable in production settings

--- shop_ai/settings/production.py
+++ shop_ai/settings/production.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
